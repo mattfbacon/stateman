c2c94695a4be4f9433deba78b6ecfde3ddbf29b9
Matt Fellenz <mattf53190@gmail.com>
2020-10-10 18:38:45 -0700

    Correct first example to match current API

--- examples/first.py
+++ examples/first.py
@@ -9,7 +9,6 @@ state.bind('foo', lambda model, changed: print(f'Foo changed to {model["foo"]}')
 state.bind('bar', lambda model, changed: print(f'Bar changed to {model["bar"]}'))
 # pass a tuple to bind to multiple properties
 state.bind(('foo', 'bar'), lambda model, changed: print(f'foo + bar = {model["foo"] + model["bar"]}'))  # args: tuple of props, handler
-# pass '*' to bind to all properties (mostly for debugging). The handler is passed two args in this case: the property that changed and, of course, the model.
-# All handlers get the property that changed if they have two args, but for single-property bindings
-# it's redundant so it's not passed if the handler takes one arg.
+# use `bind_all` bind to all properties (mostly for debugging). The handler is passed three args in this case: the event ('new' or 'changed'), the property that
+# changed, and, of course, the model.
 state.bind_all(lambda event, model, changed: print(f'Something changed! {changed} is now {model[changed]}'))
